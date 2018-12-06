f56839470b39122fc671fbec6e7938171a2a1f2c
William Alves Santos <william.alves@gmail.com>
2018-12-06 18:01:49 -0200

    Update

--- dl-scala/src/main/resources/pe/com/belcorp/datalake/resources/analytics/dwh/DCampCer/update.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO ?tableName
-SELECT
-  ANIOCAMPANA,
-  TO_DATE(FECHA, 'YYYYMMDD'),
-  PT_COUNTRY,
-  GETDATE()
-FROM ?landingSchema.tbpq_sicc_dcampcer
-WHERE PT_COUNTRY = :country
--- dl-scala/src/main/scala/pe/com/belcorp/datalake/analytics/OldTable.scala
+++
@@ -1,5 +0,0 @@
-package pe.com.belcorp.datalake.analytics
-
-import pe.com.belcorp.datalake.analytics.addons.{CreateFromResource, UpdateFromResource}
-
-trait OldTable extends Table with CreateFromResource with UpdateFromResource
